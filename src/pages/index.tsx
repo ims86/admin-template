e3b6bb4459b8994d3809b17419ee6e3f71d10e83
ismaelMS <ims.86@hotmail.com>
2022-02-17 16:26:34 -0300

    Projeto 8 - aula 163 - Estrutura inicial

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import { Children } from 'react';
+import Layout from '../components/template/Layout';
+
+export default function Home() {
+  return (
+    <Layout titulo='Titulo teste' subtitulo='Subtitulo teste'>
+      <h3>Conteudo</h3>
+    </Layout>
+  );
+}
